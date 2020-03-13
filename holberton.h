561c818df512a7e697c6efa26ee7e69b5e50dd65
michedomingo <michedomingo@gmail.com>
2020-03-13 06:49:58 -1000

    Add header file

---
+++ holberton.h
@@ -0,0 +1,36 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+
+/** specifier_functions - struct to call function based on specifier
+ * @sym: format specifier
+ * @fptr: pointer to function to print
+ */
+
+typedef struct specifier_functions
+{
+      char *sym;
+      int (*fptr)(va_list);
+} print_t;
+
+int print_char(va_list c);
+int print_string(va_list s);
+int print_percent(va_list i);
+int print_int(va_list d);
+int print_int(va_list i);
+int print_binary(va_list b);
+int print_octal(va_list o);
+int print_unsigned(va_list u);
+int print_hex(va_list x);
+int print_HEX(va_list X);
+int print_address(va_list p);
+int print_S(va_list S);
+int print_rev(va_list r);
+int print_rot13(va_list R);
+
+#endif  /* _HOLBERTON_H */
