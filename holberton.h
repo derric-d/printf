d74f9d182d57095806e29340cabae49eef6da582
michedomingo <michedomingo@gmail.com>
2020-03-14 17:08:20 -1000

    Update header

--- holberton.h
+++ holberton.h
@@ -10,20 +10,20 @@
 /**
  * struct specifier_functions - struct to choose function based on specifier
  * @sym: format specifier
- * @fptr: pointer to function to print
+ * @ptr: pointer to function to print
  */
-struct specifier_functions
+
+typedef struct specifier_functions
 {
 	char *sym;
-	int (*fptr)(va_list);
-};
-typedef struct specifier_functions print_t;
+	int (*ptr)(va_list);
+} print_t;
 
 int print_char(va_list c);
 int print_string(va_list s);
-int print_percent(va_list i);
 int print_int(va_list d);
 int print_int(va_list i);
+int print_percent(va_list i);
 int print_binary(va_list b);
 int print_octal(va_list o);
 int print_unsigned(va_list u);
@@ -33,9 +33,8 @@ int print_address(va_list p);
 int print_S(va_list S);
 int print_rev(va_list r);
 int print_rot13(va_list R);
-
 int _printf(const char *format, ...);
-int get_function(char *fmt, print_t spec_list, va_list args);
+int (*get_function(const char *fmt))(va_list);
 int _putchar(char c);
 
 #endif  /* _HOLBERTON_H */
