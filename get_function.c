e944f03660182d68a689f581a2254cac4c17e018
michedomingo <michedomingo@gmail.com>
2020-03-14 16:28:59 -1000

    Compares conversion specifier with first index of struct

--- get_function.c
+++ get_function.c
@@ -1,48 +1,24 @@
 #include "holberton.h"
 
 /**
- * get_function - finds function that we need
- * @fmt: format string passed
- * @spec_list: list of format structs we are parsing through
- * @args: arguments passed in _printf function call
- * Return: number of arguments called
+ * get_function - compares conversion specifier with first index of struct
+ * @fmt: format specifier
+ * @spec_list: array of structs of print_t type
+ * Return: pointer to valid print function
  */
-int get_function(const char *fmt, print_t spec_list[], va_list args)
+int (*get_function(const char *fmt))(va_list)
 {
-	int i, j, ret_val, numchars;
-
-	numchars = 0;
-
-	for (i = 0; fmt[i] != '\0'; i++)
+print_t spec_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i;
+	
+	for (i = 0; spec_list[i].sym != NULL; i++)
 	{
-		if (fmt[i] == '%') /* if we come across magic symbol*/
-		{
-			for (j = 0; spec_list[j].sym != NULL; j++)
-			{
-				if (fmt[i + 1] == spec_list[j].sym[0]) /*if match w/ struct sym member*/
-					ret_val = spec_list.fptr(args); /*call the func at index j if match*/
-
-				if (ret_val == -1)
-					return (-1);
-
-				numchars += ret_val;
-				break;
-			}
-			/*case no match, still string:print current char, next char &add to count*/
-			if (spec_list.sym == NULL && fmt[i + 1] == ' ')
-				if (fmt[i + 1] != '\0')
-				{
-					write_char(fmt[i]);
-					write_char(fmt[i + 1]);
-					numchars += 2;
-				}
-				return (-1);
-		}
-		else
-		{
-			write_char(fmt[i]);
-			numchars++;
-		}
+		if (*(spec_list[i].sym) == *fmt)
+			break;
 	}
-	return (numchars);
+	return (spec_list[i].ptr);
 }
